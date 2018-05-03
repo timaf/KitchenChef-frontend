1b253c2d335d8aeadddf61d31937779d73fbe6e5
Waelazar <waelazar@gmail.com>
2018-05-03 11:35:13 +0200

    add the frontend project structure

---
+++ src/main/java/at/refugeesCode/ktichencheffrontend/KtichenChefFrontendApplication.java
@@ -0,0 +1,12 @@
+package at.refugeesCode.ktichencheffrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtichenChefFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtichenChefFrontendApplication.class, args);
+	}
+}
