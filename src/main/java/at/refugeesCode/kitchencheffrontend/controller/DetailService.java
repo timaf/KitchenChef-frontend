1e6ea57aa379d797143c57ad88b628757c8e7337
Waelazar <waelazar@gmail.com>
2018-06-01 23:33:14 +0200

    comment unnecessary class

--- src/main/java/at/refugeesCode/kitchencheffrontend/controller/DetailService.java
+++ src/main/java/at/refugeesCode/kitchencheffrontend/controller/DetailService.java
@@ -11,26 +11,26 @@ import java.util.List;
 @Service
 public class DetailService {
 
-    private RestTemplate restTemplate;
-
-    @Value("${meals.url}")
-    private String mealsUrl;
-    @Value("${index.url}")
-    private String mainUrl;
-    @Value("${detail.url}")
-    private String detailUrl;
-    @Value("${shoppiglist.url}")
-    private String shoppiglistUrl;
-    @Value("${attendantsNumber.url}")
-    private String attendantsNumberUrl;
-
-    public DetailService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public List<Ingredient> showShoppingList(String id) {
-        return Arrays.asList(restTemplate.getForObject(mainUrl + detailUrl + "/" + shoppiglistUrl + "/" + id, Ingredient[].class));
-    }
+//    private RestTemplate restTemplate;
+//
+//    @Value("${meals.url}")
+//    private String mealsUrl;
+//    @Value("${index.url}")
+//    private String mainUrl;
+//    @Value("${detail.url}")
+//    private String detailUrl;
+//    @Value("${shoppiglist.url}")
+//    private String shoppiglistUrl;
+//    @Value("${attendantsNumber.url}")
+//    private String attendantsNumberUrl;
+//
+//    public DetailService(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
+//
+//    public List<Ingredient> showShoppingList(String id) {
+//        return Arrays.asList(restTemplate.getForObject(mainUrl + detailUrl + "/" + shoppiglistUrl + "/" + id, Ingredient[].class));
+//    }
 
 //    public int showAttendantsNumber(String id) {
 //        return restTemplate.getForObject(mainUrl + detailUrl + "/" + attendantsNumberUrl + "/" + id, Integer.class);
