9134459f2e7753088d8496f284db5ceb1ae08d2d
Waelazar <waelazar@gmail.com>
2018-06-03 11:00:36 +0200

    create Idess class

--- src/main/java/at/refugeesCode/kitchencheffrontend/persistence/model/Idess.java
+++ src/main/java/at/refugeesCode/kitchencheffrontend/persistence/model/Idess.java
@@ -1,4 +1,28 @@
 package at.refugeesCode.kitchencheffrontend.persistence.model;
 
 public class Idess {
+
+    private String id;
+
+    public Idess(String id) {
+        this.id = id;
+    }
+
+    public Idess() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Idess{" +
+                "id='" + id + '\'' +
+                '}';
+    }
 }
