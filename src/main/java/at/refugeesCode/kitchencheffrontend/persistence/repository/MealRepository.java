f6ac645bd2b7f8e61afe76635a7ef629f899938f
Waelazar <waelazar@gmail.com>
2018-06-03 11:02:06 +0200

    create Meal repo

--- src/main/java/at/refugeesCode/kitchencheffrontend/persistence/repository/MealRepository.java
+++ src/main/java/at/refugeesCode/kitchencheffrontend/persistence/repository/MealRepository.java
@@ -1,4 +1,8 @@
 package at.refugeesCode.kitchencheffrontend.persistence.repository;
 
-public interface MealRepository {
+import at.refugeesCode.kitchencheffrontend.persistence.model.Meal;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MealRepository extends MongoRepository<Meal, String> {
+
 }
